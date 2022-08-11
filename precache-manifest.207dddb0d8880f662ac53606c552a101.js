0f61d3f800ccfd98d49b9622553a4aa6467dbb1d
Akash Kumar <iamakash3nd@gmail.com>
2022-08-11 21:58:31 +0530

    Updates

---
+++ precache-manifest.207dddb0d8880f662ac53606c552a101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e55f72143eb04a9be42b",
+    "url": "/my-Text-Utils/static/css/main.1be92cd1.chunk.css"
+  },
+  {
+    "revision": "e55f72143eb04a9be42b",
+    "url": "/my-Text-Utils/static/js/main.e55f7214.chunk.js"
+  },
+  {
+    "revision": "7ab2393a9ad8c2b58dcd",
+    "url": "/my-Text-Utils/static/js/1.7ab2393a.chunk.js"
+  },
+  {
+    "revision": "1b0ef82739d6ea87279e",
+    "url": "/my-Text-Utils/static/js/2.1b0ef827.chunk.js"
+  },
+  {
+    "revision": "db758532dbacfe4b3b80",
+    "url": "/my-Text-Utils/static/js/runtime~main.db758532.js"
+  },
+  {
+    "revision": "ba8bd230ef238a76c03a78bed6dcebf4",
+    "url": "/my-Text-Utils/index.html"
+  }
+];
